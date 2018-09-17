078b91c1917d2bb3c2588d8fad0e8eb392227add
Oliver Baum <oliver@baums.org.uk>
2018-09-17 18:07:30 +0100

    used form handled by sinatra to create POST request

--- web-app/app.rb
+++ web-app/app.rb
@@ -13,6 +13,17 @@ get '/anotherRoute' do
   "Welcome to the other route"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @name = params[:name]
+  erb(:index)
+end
+
+post '/named-cat' do
+  @name = params[:name]
   erb(:index)
 end
